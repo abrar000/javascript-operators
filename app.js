21b7d496498833dd313ac44454dfe05d48b368e9
abrar000 <abrar@uconnect.pk>
2018-12-12 12:40:37 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+// arttemetic operators
+
+var a = 20;
+var b = 10;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a++ );
+console.log(a-- );
+
+// console.log(b++);
+// console.log(a-- );
+// console.log(b--);
+
+// Assignment operators
+
+console.log( a = b); // a = 10 b = 10
+console.log(a += b); // a = a + b => 20
+console.log(a -= b); // a = a - b => 10
+console.log(a *= b); // a = a * b => 100
+console.log(a /= b); // a = a / b => 10
+console.log(a % b); // a = a % b => 0
+
+// Comparision operators
+var x =5;
+var y =10;
+console.log(x == y); // equal
+console.log(x != y); // not equal
+console.log(x > y); // greater than
+console.log(x < y); // less than
+console.log (x >= y); // greater than or equal to
+console.log (x <= y); // less than or equal to
+
+// logic operators
+
+var a = true;
+var b = false;
+console.log(a && b); // and operator
+console.log(a || b); // or operator
+console.log(!(a || b)); //reverse
+
+// string operators
+
+var a = "abrar";
+var b = "hussain"
+console.log( a + b); // concatenate
